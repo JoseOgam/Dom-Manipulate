7b17f7e5b36fde4c8f0c38c471fc4838400d8fcb
JoseOgam <jotienoochieng@gmail.com>
2020-08-05 14:30:45 -0400

    h3  element created

--- scripts.js
+++ scripts.js
@@ -2,3 +2,7 @@ var headerTag = document.createElement("h1");
 headerTag.textContent = "DOM Manipulation";
 
 document.querySelector("#app").appendChild(headerTag);
+
+var header3 = document.createElement("h3");
+header3.textContent = "Registration Form";
+document.querySelector(".form-element").appendChild(header3);
