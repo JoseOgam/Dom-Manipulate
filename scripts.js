4620a2038ab7612a5efd6e9ee411aaaca50711d9
JoseOgam <jotienoochieng@gmail.com>
2020-08-05 15:30:37 -0400

    index and scripts files updated

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 var headerTag = document.createElement("h1");
 headerTag.textContent = "DOM Manipulation";
+headerTag.style.color = "blue";
 
-document.querySelector("#app").appendChild(headerTag);
+document.querySelector(".container").appendChild(headerTag);
 
 var header3 = document.createElement("h3");
-header3.textContent = "Registration Form";
-document.querySelector(".form-element").appendChild(header3);
+header3.textContent = "Todo list";
+header3.style.cssText = "color: brown";
+document.querySelector(".container").appendChild(header3);
