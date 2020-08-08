57ec8160adb77488bdf5d4779fe08eaea90474a8
JoseOgam <jotienoochieng@gmail.com>
2020-08-08 08:02:47 -0400

    fixed bugs that prevented text display on the browser

---
+++ scripts.js
@@ -0,0 +1,24 @@
+let addTodo = document.querySelector(".addTodo");
+let deleteIcon = document.querySelector(".deleteIcon");
+let todosList = document.querySelector(".todosList");
+let searchText = document.querySelector("searchText");
+
+let createTemplate = (todo) => {
+  var htmlEl = ` <li
+          class="list-group-item d-flex justify-content-between align-items-center"
+        >
+          <span>${todo}</span>
+          <i class="far fa-trash-alt delete"></i>
+        </li>`;
+  todosList.innerHTML += htmlEl;
+};
+
+addTodo.addEventListener("submit", (e) => {
+  e.preventDefault();
+  var todo = addTodo.add.value.trim();
+
+  if (todo.length) {
+    createTemplate(todo);
+    addTodo.reset();
+  }
+});
